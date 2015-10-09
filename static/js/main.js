fea22c1611fd5f5a0fa0447e2ba3dacec75a55c7
Jakub Dakowicz <jakubdakowicz@Mac-mini-Kuba.local>
2015-10-09 15:50:06 +0200

    working sample js

--- static/js/main.js
+++ static/js/main.js
@@ -4,9 +4,29 @@
         $body: null,
         init: function () {
             this.$body = $('body');
+            this.toggleMenu();
+            this.submenus();
         },
         breakpoint: function () {
             return window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content').replace(/\"/g, '');
+        },
+        toggleMenu: function () {
+            $('.js-menu-toggler').on('click', function (e) {
+                e.preventDefault();
+                $('.sec--nav').toggleClass('active')
+                $('body').toggleClass('blocked');
+            })
+        },
+        submenus: function () {
+            var $submenu = $('.submenu');
+            $('.js-with-submenu a').on('click', function (e) {
+                if ($(this).siblings('.submenu').is('.active')) {
+                    $(this).siblings('.submenu').toggleClass('active');
+                } else {
+                    $submenu.removeClass('active');
+                    $(this).siblings('.submenu').addClass('active');
+                }
+            })
         }
     };
     $(document).on('ready', function () {
