1cc06db1d3e8a89f47502c813cc5baf0daad8f11
Stoneski93 <nor.kam@vp.pl>
2015-11-16 09:48:36 +0100

    Norbert changes

--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,38 @@
         $body: null,
         init: function () {
             this.$body = $('body');
+            this.dropdownMenu();
+            this.dropdownSubmenu();
+            this.navResize();
         },
         breakpoint: function () {
             return window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content').replace(/\"/g, '');
+        },
+        dropdownMenu: function () {
+                $('.js-menu-toggler').on('click', function (e) {
+                        $('.sec--nav').toggleClass('opened');
+                        $('body').toggleClass('menu-opened');      
+            });
+        },
+        dropdownSubmenu: function () {
+                $('.js-menu-item-wrapper').on('click', function (e) {
+                    $(this).siblings().removeClass('active');
+                    if($(this).hasClass('submenu-point')){
+                        e.preventDefault();
+                        $(this).toggleClass('active');
+                    }
+                $
+
+            });
+        },
+        navResize: function () {
+            $(window).on('resize', function(e) {
+                
+            });
         }
+
     };
+
     $(document).on('ready', function () {
         window.CHANGE_ME.init();
     });
