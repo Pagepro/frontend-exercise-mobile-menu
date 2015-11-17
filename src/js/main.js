5a512c0160fc795580770e69d6736ffbc79a0bcc
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2015-11-17 12:17:30 +0100

    CODEREVIEW - add comments

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 (function ($, window, document) {
     "use strict";
-    window.CHANGE_ME = window.CHANGE_ME || {
+    window.CHANGE_ME = window.CHANGE_ME || { // AD: `CHANGE_ME` should be changed into project name
         $body: null,
         init: function () {
             this.$body = $('body');
@@ -13,30 +13,33 @@
         },
         dropdownMenu: function () {
                 $('.js-menu-toggler').on('click', function (e) {
+                    // AD: `e.preventDefault()` is missing here
                         $('.sec--nav').toggleClass('opened');
-                        $('body').toggleClass('menu-opened');      
+                        $('body').toggleClass('menu-opened');
+                    // AD: try to use correct indent. Two lines above are too deep.
             });
         },
         dropdownSubmenu: function () {
                 $('.js-menu-item-wrapper').on('click', function (e) {
                     $(this).siblings().removeClass('active');
-                    if($(this).hasClass('submenu-point')){
+                    if($(this).hasClass('submenu-point')){ // AD: put spaces after `if` keyword and before `{`
                         e.preventDefault();
                         $(this).toggleClass('active');
                     }
-                $
+                $ // AD: why this is here?
 
             });
         },
         navResize: function () {
+            // AD: do you need this function? why it's here? If you don't need function which doesn't do anything - delete it.
             $(window).on('resize', function(e) {
-                
+
             });
         }
 
     };
 
     $(document).on('ready', function () {
-        window.CHANGE_ME.init();
+        window.CHANGE_ME.init(); // AD: `CHANGE_ME` should be changed into project name
     });
 }(jQuery, window, document));
